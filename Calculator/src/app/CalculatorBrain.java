051e81b246acc474d8950d5eac2a0da781d16766
julio rosario <rosariojulio40@gmail.com>
2017-12-22 10:29:55 -0500

    Finished Calculator

---
+++ Calculator/src/app/CalculatorBrain.java
@@ -0,0 +1,159 @@
+package app;
+
+
+public class CalculatorBrain {
+	
+	private double accumulator;
+	private String mathSymbol;
+	private boolean flag;
+	
+	
+	/**
+	 * no-args Constructor
+	 */
+	CalculatorBrain(){
+		accumulator = 0;
+		mathSymbol = "";
+		flag = false;
+	}
+	
+	/**
+	 * The setOperand method set the accumulator
+	 * equal to the operand
+	 * @param number an operand enter by the user
+	 */
+	public void setOperand(double number) {
+		accumulator = number;
+		flag = true;
+	}
+	
+	/**
+	 * The setOperator method set the 
+	 * field mathSymbol equal to symbol
+	 * @param symbol a math symbol for 
+	 * arithmetic operation
+	 */
+	public void setOperator(String symbol) {
+		mathSymbol = symbol;
+	}
+	
+	/**
+	 * The performUnaryOperation method 
+	 * perform a unary operation of the 
+	 * accumulator if there is one
+	 */
+	public void performUnaryOperation() {
+		if(set()) {
+			if(mathSymbol.equals("cos")) 
+	            accumulator = Math.cos(accumulator);
+			else if(mathSymbol.equals("sin"))
+				accumulator = Math.sin(accumulator);
+			else if(mathSymbol.equals("tan"))
+				accumulator = Math.tan(accumulator);
+			else if(mathSymbol.equals("(-)"))
+				   accumulator *=-1;
+			else if(mathSymbol.equals("PI"))
+				   accumulator = Math.PI;
+			else if(mathSymbol.equals("log"))
+				accumulator = Math.log(accumulator);
+			else if(mathSymbol.equals("ln")) 
+				accumulator = Math.log1p(accumulator-1);
+			else if(mathSymbol.equals("sqrt")) {
+				if(accumulator >=0)
+					accumulator = Math.sqrt(accumulator);
+			}else if(mathSymbol.equals("clear"))
+				clear();	
+	  }
+	}
+	
+	/**
+	 * The performBinaryOperation method perform 
+	 * a binary operation between two operand 
+	 * where accumulator is operand 1 and 
+	 * @param number is operand 2 of the binary operation
+	 */
+	public void performBinaryOperation(double number) {
+		
+		if(set()) {
+			if(mathSymbol.equals("/"))
+				divide(number);
+			else if(mathSymbol.equals("x"))
+				multiply(number);
+			else if(mathSymbol.equals("+"))
+				add(number);
+			else if(mathSymbol.equals("-"))
+				subtract(number);
+		}
+	}
+	
+	/**
+	 * The add method receives a 
+	 * @param number an operand and add it to
+	 * the field accumulator
+	 */
+	public void add(double number) {
+		accumulator += number;
+	}
+	
+	/**
+	 * The subtract method receives a 
+	 * @param number an operand and subtract it
+	 * from the accumulator
+	 */
+	public void subtract(double number) {
+		accumulator -= number;
+	}
+	
+	/**
+	 * The multiply method receives a 
+	 * @param number an operand and multiply it
+	 * by the field accumulator and store it in it.
+	 */
+	public void multiply(double number) {
+		accumulator*=number;
+	}
+	
+	/**
+	 * The divide method receives a 
+	 * @param number an operand that is 
+	 * divide it by the field accumulator
+	 */
+	public void divide(double number) {
+		if(number!= 0)
+			accumulator/=number;
+	}	
+	
+	/**
+	 * The getResult method 
+	 * @return the accumulator of calculations
+	 */
+	public String  getResult() {
+		String tmp1 = String.valueOf(accumulator);
+		String decimals = tmp1.substring(tmp1.indexOf(".")+1);
+		
+		if(decimals.charAt(0) != '0' || decimals.length()>=2) 
+			return tmp1;
+		else
+			 return tmp1.substring(0, tmp1.indexOf("."));
+	}
+	
+	/**
+	 * The set method 
+	 * @return whether or not the first operand 
+	 * has been set.
+	 */
+	public boolean set() {
+		return flag;
+	}
+	
+	/**
+	 * The clear method restore 
+	 * all the field of this class
+	 * to default values.
+	 */
+	public void clear() {
+		flag = false;
+		accumulator = 0;
+		mathSymbol = "";
+	}
+}
---
+++ Calculator/src/app/Main.java
@@ -0,0 +1,116 @@
+/**
+ * Author: Julio Rosario
+ * Last Update: 12.20.17
+ * Purpose: The purpose of this program is to create a basic calculator that 
+ *          perform basic calculation such as addition,subtraction,multiplication
+ *          and division, as well as unary operation such as cosine,sine and tangent.
+ */
+
+package app;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+
+public class Main extends Application {
+	
+	@FXML
+	private Label display;
+	private boolean userIsTyping;
+	private CalculatorBrain brain;
+	
+	
+	/*******************************
+	 * The initialize method 
+	 * initialize the data
+	 * of this class.
+	 *********************************/
+	public void initialize() {
+		userIsTyping = false;
+		brain = new CalculatorBrain();
+	}
+	
+	/*********************************************
+	 * The pressedDigit method receives a
+	 * @param event which is call whenever the user
+	 * pressed a digit.
+	 **********************************************/
+	public void pressedDigit(ActionEvent event) {
+		Button digit =(Button) event.getSource();
+		
+		if(!userIsTyping) {
+			display.setText(digit.getText());
+		}
+		else{
+			String tmp = display.getText();
+			tmp += digit.getText();
+			display.setText(tmp);
+		}
+		
+		userIsTyping = true;
+	}
+	
+	/*******************************************
+	 * The performCalculation method receives 
+	 * @param event is an operator, this method
+	 * is triggered every time the user pressed
+	 * one of the operators of the calculator.
+	 ********************************************/
+	public void performCalculation(ActionEvent event) {
+		
+		//Get the operator button pressed by the user 
+		//and set operator.
+		Button operator = (Button)event.getSource();
+		double number = Double.parseDouble(display.getText());
+		
+		//If the first operand has not been set operand
+		if(!brain.set()) 
+			brain.setOperand(number);
+		
+		if(userIsTyping && brain.set())
+			//brain.performUnaryOperation();
+			brain.performBinaryOperation(number);
+	
+		//Set operator and if there is a unary operation do it
+		brain.setOperator(operator.getText());
+		brain.performUnaryOperation();
+		
+		//Display result
+		display.setText(brain.getResult());
+		userIsTyping = false;
+	}
+	
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		initialize();
+		
+		//Load the FXML file
+		Parent parent = FXMLLoader.load(
+				 getClass().getResource("CalculatorView.fxml"));
+		
+		//Build the scene graph
+		Scene scene = new Scene(parent);
+		
+		//Display the window using the scene graph
+	    stage.setTitle("Calculator");
+	    stage.setScene(scene);
+	    stage.show();
+	}
+	
+	public static void main(String[]args) {
+		
+		//Launch the application
+		launch(args);
+	}
+	
+
+}
